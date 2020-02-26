d89520249fe1e28ff0198c0eda9773da7f124458
mantismb <matt.d.baldwin@gmail.com>
2020-02-25 19:59:36 -0800

    Started Site

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,60 +1,67 @@
 let resumeData = {
-    "imagebaseurl":"https://rbhatia46.github.io/",
-    "name": "Rahul Bhatia",
-    "role": "Frontend Developer and Data Scientist",
-    "linkedinId":"Your LinkedIn Id",
+    "imagebaseurl":"https://github.com/matthewdbaldwin",
+    "name": "Matthew Baldwin",
+    "role": "Frontend Developer and Marketer",
+    "linkedinId":"matthewdbaldwin",
     "skypeid": "Your skypeid",
-    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "roleDescription": " I like dabbling in various parts of frontend development and like to learn about new technologies, make creative designs, and manage campaigns.",
     "socialLinks":[
         {
           "name":"linkedin",
-          "url":"https://www.linkedin.com/in/rahul-bhatia-67ba08121/",
+          "url":"https://www.linkedin.com/in/mattdbaldwin/",
           "className":"fa fa-linkedin"
         },
         {
           "name":"github",
-          "url":"http://github.com/rbhatia46",
+          "url":"https://github.com/matthewdbaldwin",
           "className":"fa fa-github"
         },
-        {
-          "name":"skype",
-          "url":"http://twitter.com/rbhatia46",
-          "className":"fa fa-twitter"
-        }
+
       ],
-    "aboutme":"I am currently a pre-final year student at The LNM Institute of Information Technology and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
-    "address":"India",
-    "website":"https://rbhatia46.github.io",
+    "aboutme": `I’m Matthew Baldwin, a full-stack web developer. I am passionate about programming and using new development tools and technology. For the past seven years I have worked in a corporate healthcare data industry involved in digital marketing campaigns.
+    
+    My previous experiences included analyzing customer experiences through website landing pages, content creation and email automation tools such as Pardot and Eloqua. I have over a decade of experience with Adobe Suite creating content in Photoshop, Illustrator and InDesign. I have managed websites as part of my marketing duties and developed them using HTML, CSS, PHP, Javascript and managing CMS platforms. This allowed me to create a passion for coding and developing websites and their technologies. I was fortunate to take on more responsibility coding and developing these corporate websites and learned more about JavaScript and project management platforms. 
+    
+    I needed a way to learn more and enrolled at LearningFuze, and take night classes while still holding my job. While at Learning Fuze I was given challenging programming problems to solve. Engaging me and exposing me to more technologies such as JavaScript, jQuery, React and NodeJs. My knowledge grew as a front end developer as I worked on several React applications. NodeJs and Express exposed me to working as a back-end developer as I edited databases and made even more progress. 
+    
+    My experience managing campaigns for customer experiences combined with my skills as a Web Developer would make me a fantastic asset to your team. I would love the opportunity to bring my passion and dedication to web development to your company.`,
+    "address":"Orange County, CA",
+    "website":"https://github.com/matthewdbaldwin",
     "education":[
       {
-        "UniversityName":"The LNM Insitute of Information Technology",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Aug",
-        "YearOfPassing":"2020",
-        "Achievements":"Some Achievements"
+        "UniversityName":"California State Polytechnic University Pomona",
+        "specialization":"Business Administration",
+        "MonthOfPassing":"June",
+        "YearOfPassing":"2008",
+        "Achievements":"Brand Management"
       },
       {
-        "UniversityName":"Some University",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Jan",
-        "YearOfPassing":"2018",
-        "Achievements":"Some Achievements"
+        "UniversityName":"Learning Fuze",
+        "specialization":"Full-stack development program is a 12-week intensive that teaches front-end and back-end subjects like JavaScript/jQuery, React, Node.js, PHP, MySQL, HTML, CSS, Amazon Web Services (AWS), Git/GitHub, and others.",
+        "MonthOfPassing":"March",
+        "YearOfPassing":"2020",
+        "Achievements":"Completed Full-stack development courses"
       }
     ],
     "work":[
       {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
+        "CompanyName":"IQVIA",
+        "specialization":"Marketing Manager",
+        "MonthOfLeaving":"",
+        "YearOfLeaving":"Present",
+        "Achievements":`Develop OneKeyData.com utilizing ExpressionEngine to publish content and development. Lead site development with Nodejs, and tracked through Beanstalk and GitHub using HTML, CSS, Javascript, JSON, ECMA 5 & 6 and PHP. Collaborate with 3rd party development team Lucid Fusion. Utilize Microsoft Sharepoint CMS and SQL for U.S. marketing on IQVIA.com to create product pages and campaign landing pages. Lead identification of KPI's through Google suite of products.
+        `
       },
       {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
+        "CompanyName":"Cegedim",
+        "specialization":"Marketing Specialist",
+        "MonthOfLeaving":"April",
+        "YearOfLeaving":"2015",
+        "Achievements":`Designed and deployed email campaigns through Marketo.
+
+        Created product pages and campaign landing pages for US marketing on cegedimrm.com. Updated and maintained websites with Microsoft Sharepoint using Javascript, PHP and HTML.
+        
+        Designed layouts for print and digital media including brochures, fact sheets, count sheets, banners, newsletters, advertiesments, and other sales collateral with Photoshop and Illustrator.`
       }
     ],
     "skillsDescription":"Your skills here",
@@ -63,42 +70,40 @@ let resumeData = {
         "skillname":"HTML5"
       },
       {
-        "skillname":"CSS"
+        "skillname":"CSS3"
       },
       {
-        "skillname":"Reactjs"
+        "skillname":"JavaScript"
+      },
+      {
+        "skillname":"React.js"
       }
     ],
     "portfolio":[
       {
-        "name":"project1",
-        "description":"mobileapp",
+        "name":"OneKeyData.com",
+        "description":"website",
         "imgurl":"images/portfolio/phone.jpg"
       },
       {
-        "name":"project2",
-        "description":"mobileapp",
+        "name":"winwinbrewery.com",
+        "description":"website",
         "imgurl":"images/portfolio/project.jpg"
       },
       {
-        "name":"project3",
-        "description":"mobileapp",  
+        "name":"matthewdbaldwin.com",
+        "description":"website",  
         "imgurl":"images/portfolio/project2.png"
-      },
-      {
-        "name":"project4",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
       }
     ],
     "testimonials":[
       {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
+        "description":"Matt has a lot of technical skills",
+        "name":"Mattias Huber"
       },
       {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
+        "description":"Phenomenal to work with.",
+        "name":"Kenneth Hope"
       }
     ]
   }
