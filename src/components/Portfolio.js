e85805330b11f03e79f2e995450628545322d89d
Matthew Baldwin <matt.d.baldwin@gmail.com>
2023-08-23 00:51:58 -0700

    js upgrades

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -11,19 +11,19 @@ export default class Portfolio extends Component {
           {
             resumeData.portfolio && resumeData.portfolio.map((item)=>{
               return(
-                <div className="columns portfolio-item">
-                  <div className="item-wrap">
-                    <a href={item.url}>
-                      <img src={`${item.imgurl}`} alt={`${item.alt}`} className="item-img"/>
-                      <div className="overlay">
-                        <div className="portfolio-item-meta">
-                          <h5>{item.name}</h5>
-                          <p>{item.description}</p>
+                <a href={item.url}>
+                  <div className="columns portfolio-item">
+                    <div className="item-wrap">
+                        <img src={`${item.imgurl}`} alt={`${item.alt}`} className="item-img"/>
+                        <div className="overlay">
+                          <div className="portfolio-item-meta">
+                            <h5>{item.name}</h5>
+                            <p>{item.description}</p>
+                          </div>
                         </div>
-                      </div>
-                    </a>
+                    </div>
                   </div>
-                </div>
+                </a>
               )
             })
           }
