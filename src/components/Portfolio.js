295091604dc6096abfa8b58e923fa8edd866f5aa
Matthew Baldwin <matt.d.baldwin@gmail.com>
2025-08-29 09:08:12 -0700

    Changed portfolio.js to include light boxes

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -3,10 +3,7 @@ import Lightbox from 'react-image-lightbox';
 import 'react-image-lightbox/style.css';
 
 export default class Portfolio extends Component {
-  state = {
-    isOpen: false,
-    photoIndex: 0,
-  };
+  state = { isOpen: false, photoIndex: 0 };
 
   openLightbox = (photoIndex, e) => {
     if (e) e.preventDefault();
@@ -17,87 +14,75 @@ export default class Portfolio extends Component {
     const { resumeData } = this.props;
     const { isOpen, photoIndex } = this.state;
 
-    const listA = resumeData.portfolio0 || [];
-    const listB = resumeData.portfolio || [];
-    const images = [
-      ...listA.map((i) => i.imgurl),
-      ...listB.map((i) => i.imgurl),
-    ];
-    const titles = [
-      ...listA.map((i) => i.name),
-      ...listB.map((i) => i.name),
-    ];
-    const captions = [
-      ...listA.map((i) => i.description),
-      ...listB.map((i) => i.description),
-    ];
+    const listA = resumeData.portfolio0 || []; // Samples of Work
+    const listB = resumeData.portfolio || [];  // Websites
+
+    // Only images from listA will be in the lightbox
+    const images   = listA.map(i => i.imgurl);
+    const titles   = listA.map(i => i.name);
+    const captions = listA.map(i => i.description);
 
     const total = images.length;
-    const nextIndex = (idx) => (idx + 1) % total;
-    const prevIndex = (idx) => (idx + total - 1) % total;
+    const nextIndex = (i) => (i + 1) % total;
+    const prevIndex = (i) => (i + total - 1) % total;
 
     return (
       <section id="portfolio">
-        {/* Samples of Work */}
+        {/* Samples of Work → Lightbox */}
         <div className="row">
           <div className="twelve columns collapsed">
             <h1>Samples of Work</h1>
-            <div id="portfolio-wrapper-0" className="bgrid-quarters s-bgrid-thirds cf">
-              {listA.map((item, i) => {
-                const globalIndex = i; // first block starts at 0
-                return (
-                  <a
-                    key={item.id}
-                    href={item.imgurl}
-                    title={item.name}
-                    onClick={(e) => this.openLightbox(globalIndex, e)}
-                  >
-                    <div className="columns portfolio-item">
-                      <div className="item-wrap">
-                        <img src={item.imgurl} alt={item.alt} className="item-img" />
-                        <div className="overlay">
-                          <div className="portfolio-item-meta">
-                            <h5>{item.name}</h5>
-                            <p>{item.description}</p>
-                          </div>
+            <div id="portfolio-grid-0" className="bgrid-quarters s-bgrid-thirds cf">
+              {listA.map((item, i) => (
+                <a
+                  key={item.id}
+                  href={item.imgurl}
+                  title={item.name}
+                  onClick={(e) => this.openLightbox(i, e)}
+                >
+                  <div className="columns portfolio-item">
+                    <div className="item-wrap">
+                      <img src={item.imgurl} alt={item.alt} className="item-img" />
+                      <div className="overlay">
+                        <div className="portfolio-item-meta">
+                          <h5>{item.name}</h5>
+                          <p>{item.description}</p>
                         </div>
                       </div>
                     </div>
-                  </a>
-                );
-              })}
+                  </div>
+                </a>
+              ))}
             </div>
           </div>
         </div>
 
-        {/* Websites */}
+        {/* Websites → Direct links */}
         <div className="row">
           <div className="twelve columns collapsed">
             <h1>Websites</h1>
-            <div id="portfolio-wrapper-1" className="bgrid-quarters s-bgrid-thirds cf">
-              {listB.map((item, i) => {
-                const globalIndex = (listA.length) + i; // offset by first block length
-                return (
-                  <a
-                    key={item.id}
-                    href={item.imgurl}
-                    title={item.name}
-                    onClick={(e) => this.openLightbox(globalIndex, e)}
-                  >
-                    <div className="columns portfolio-item">
-                      <div className="item-wrap">
-                        <img src={item.imgurl} alt={item.alt} className="item-img" />
-                        <div className="overlay">
-                          <div className="portfolio-item-meta">
-                            <h5>{item.name}</h5>
-                            <p>{item.description}</p>
-                          </div>
+            <div id="portfolio-grid-1" className="bgrid-quarters s-bgrid-thirds cf">
+              {listB.map((item) => (
+                <a
+                  key={item.id}
+                  href={item.url}
+                  title={item.name}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <div className="columns portfolio-item">
+                    <div className="item-wrap">
+                      <img src={item.imgurl} alt={item.alt} className="item-img" />
+                      <div className="overlay">
+                        <div className="portfolio-item-meta">
+                          <h5>{item.name}</h5>
+                          <p>{item.description}</p>
                         </div>
                       </div>
                     </div>
-                  </a>
-                );
-              })}
+                  </div>
+                </a>
+              ))}
             </div>
           </div>
         </div>
@@ -108,15 +93,11 @@ export default class Portfolio extends Component {
             nextSrc={images[nextIndex(photoIndex)]}
             prevSrc={images[prevIndex(photoIndex)]}
             onCloseRequest={() => this.setState({ isOpen: false })}
-            onMovePrevRequest={() =>
-              this.setState({ photoIndex: prevIndex(photoIndex) })
-            }
-            onMoveNextRequest={() =>
-              this.setState({ photoIndex: nextIndex(photoIndex) })
-            }
+            onMovePrevRequest={() => this.setState({ photoIndex: prevIndex(photoIndex) })}
+            onMoveNextRequest={() => this.setState({ photoIndex: nextIndex(photoIndex) })}
             imageTitle={titles[photoIndex]}
             imageCaption={captions[photoIndex]}
-            enableZoom={true}
+            enableZoom
           />
         )}
       </section>
