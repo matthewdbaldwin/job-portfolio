466a419d0e258a406fd78b4a432f3db78790d0f7
Matthew Baldwin <matt.d.baldwin@gmail.com>
2025-08-28 23:58:32 -0700

    Revised resumedata

--- src/resumeData.js
+++ src/resumeData.js
@@ -222,6 +222,13 @@ let resumeData = {
         "role":"Marketing Director",
         "company":"Profound Logic Software"
       },
+            {
+        "id":"BV",
+        "description":"Leading in Marketing is your strength.",
+        "name":"Eric Strickland",
+        "role":"Senior Consultant",
+        "company":"Black and Veatch"
+      },
       {
         "id":"QI",
         "description":"Matt has a lot of technical skills.",
