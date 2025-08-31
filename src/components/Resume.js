4e86ab241be4d9fac378e1b4756603a8a0ece60c
Matthew Baldwin <matt.d.baldwin@gmail.com>
2025-08-31 12:25:59 -0700

    housekeeping/indenting

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -4,36 +4,10 @@ export default  class Resume extends Component {
     let resumeData = this.props.resumeData;
     return (
       <section id="resume">
-
-         <div className="row education">
-
-            <div className="three columns header-col">
-               <h1><span>Education</span></h1>
-            </div>
-
-            <div className="nine columns main-col">
-              {
-                resumeData.education && resumeData.education.map((item)=>{
-                  return(
-                    <div key={item.id} className="row item">
-                       <div className="twelve columns">
-                          <h3>{item.UniversityName}</h3>
-                          <p className="info">
-                          {item.specialization}<br/>
-                          {item.Achievements}
-                          </p>
-                       </div>
-                    </div>
-                  )
-                })
-              }
-            </div>
-         </div>
         <div className="row work">
-            <div className="three columns header-col">
-               <h1><span>Work</span></h1>
-            </div>
-
+          <div className="three columns header-col">
+            <h1><span>Work</span></h1>
+          </div>
             <div className="nine columns main-col">
               {
                 resumeData.work && resumeData.work.map((item) => {
@@ -57,42 +31,53 @@ export default  class Resume extends Component {
                 })
               }
             </div> 
+          </div>
+          <div className="row education">
+            <div className="three columns header-col">
+               <h1><span>Education</span></h1>
+            </div>
+            <div className="nine columns main-col">
+              {
+                resumeData.education && resumeData.education.map((item)=>{
+                  return(
+                    <div key={item.id} className="row item">
+                       <div className="twelve columns">
+                          <h3>{item.UniversityName}</h3>
+                          <p className="info">
+                          {item.specialization}<br/>
+                          {item.Achievements}
+                          </p>
+                       </div>
+                    </div>
+                  )
+                })
+              }
+            </div>
          </div>
-
          <div className="row skill">
-
             <div className="three columns header-col">
                <h1><span>Skills</span></h1>
             </div>
-
             <div className="nine columns main-col">
-
                <p>
                {resumeData.skillsDescription}
                </p>
-
-   				<div className="bars">
-
-   				   <ul className="skills">
-                {
-                  resumeData.skills && resumeData.skills.map((item) => {
-                    return(
-                      <li key={item.id}>
-                      <span className={`bar-expand ${item.id.toLowerCase()}`}>
-                      </span><em>{item.skillname}</em>
-                      </li>
-                    )
-                  })
-                }
-
-   					</ul>
-
-   				</div>
-
-   			</div>
-
-         </div>
-
+              <div className="bars">
+                <ul className="skills">
+                    {
+                      resumeData.skills && resumeData.skills.map((item) => {
+                        return(
+                          <li key={item.id}>
+                          <span className={`bar-expand ${item.id.toLowerCase()}`}>
+                          </span><em>{item.skillname}</em>
+                          </li>
+                        )
+                      })
+                    }
+                </ul>
+              </div>
+            </div>
+          </div>
       </section>
     );
   }
