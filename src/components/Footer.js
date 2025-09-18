e50d086737427a6ebd1ad8ac98d4327eb87367da
Matthew Baldwin <matt.d.baldwin@gmail.com>
2025-09-17 20:43:55 -0700

    Redesigned bottom half of the site

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,33 +1,38 @@
 import React, { Component } from 'react';
+
 export default class Footer extends Component {
   render() {
-    let resumeData = this.props.resumeData;
+    let resumeData = this.props.resumeData || { socialLinks: [] };
+    const year = new Date().getFullYear();
+
     return (
       <footer>
         <div className="row">
           <div className="twelve columns">
             <ul className="social-links">
-              {
-                resumeData.socialLinks && resumeData.socialLinks.map((item)=>{
-                  return(
-                    <li key={item.id}>
-                      <a href={item.url}>
-                      <i className={item.className} />
-                      </a>
-                    </li>
-                  )
-                })
-              }
+              {resumeData.socialLinks && resumeData.socialLinks.map((item, i) => (
+                <li key={i}>
+                  <a href={item.url} target="_blank" rel="noopener noreferrer" aria-label={item.name || 'social'}>
+                    <i className={item.className}></i>
+                  </a>
+                </li>
+              ))}
             </ul>
-            
           </div>
-          <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open" /></a></div>
+
+          <div id="go-top">
+            <a className="smoothscroll" title="Back to Top" href="#home" aria-label="Back to top">
+              <i className="icon-up-open" />
+            </a>
+          </div>
         </div>
+
         <div className="row">
           <div className="twelve columns">
-              <p>Copyright Matthew Baldwin <script>document.write(new Date().getFullYear())</script>
-              </p>
-              <p>No cookies or data are collected on this site.</p>
+            <ul className="copyright">
+              <li>© {year} Matthew Baldwin — {resumeData?.address}</li>
+              <li>No cookies or data are collected on this site.</li>
+            </ul>
           </div>
         </div>
       </footer>
