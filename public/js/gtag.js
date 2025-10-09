0a11f2b133aa7e43f9f2898eb29913cdca05d802
Matthew Baldwin <47402880+matthewdbaldwin@users.noreply.github.com>
2025-10-09 16:30:16 -0700

    Keep testimonials background unobstructed

---
+++ public/js/gtag.js
@@ -0,0 +1,11 @@
+'use strict';
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){ window.dataLayer.push(arguments); }
+
+gtag('js', new Date());
+
+gtag('config', 'G-L82Y5HVQN1', {
+  anonymize_ip: true,
+  allow_google_signals: false,
+});
